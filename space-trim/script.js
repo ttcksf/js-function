9c55677934d67f340cfb040883490776ff3c880a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-11-23 17:18:09 +0900

    add space-trim

---
+++ space-trim/script.js
@@ -0,0 +1,14 @@
+const targetString1 = "　こんにちは ";
+const trimmedString1 = targetString1.trim();
+console.log(targetString1);
+console.log(trimmedString1);
+
+const targetString2 = 'りんごを食べました\n\n';
+console.log(targetString2);
+const trimmedString2 = targetString2.trim();
+console.log(trimmedString2);
+
+const targetString3 = " りんご　みかん  ";
+console.log(targetString3);
+const trimmedString3 = targetString3.trim();
+console.log(trimmedString3);
