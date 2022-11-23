9b07831c20f47cd36259e2697a45bdb5d0c5e0f5
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-11-23 17:23:36 +0900

    add search-font

---
+++ search-font/script.js
@@ -0,0 +1,11 @@
+const myString = "JavaScriptを覚えよう";
+
+const a1 = "JavaScriptを覚えよう".includes("JavaScript");
+console.log(a1);
+
+const a2 = "JavaScriptを覚えよう".startsWith("JavaScript");
+console.log(a2);
+
+const a3 = "JavaScriptを覚えよう".endsWith("JavaScript");
+console.log(a3);
+
