de5c083dbc92733196d9c56ed68b24775dacf992
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-14 00:56:08 +0900

    add array-reduce

---
+++ array-reduce/script.js
@@ -0,0 +1,30 @@
+//reduce
+// const number = [100,200,300];
+
+// const sum = number.reduce((pre,current) => {
+//   return pre + current;
+// });
+
+// console.log(sum);
+
+//for
+// const number = [100,200,300];
+// let sum = 0;
+// for(const num of number){
+//   sum += num;
+// }
+// console.log(sum);
+
+//reduce 
+// const array = [["aaa","bbb"],["ccc","ddd"]];
+// const mergeArray = array.reduce((pre, current) => {
+//   return pre.concat(current);
+// });
+// console.log(mergeArray);
+
+//reduceRight
+const array = [["aaa","bbb"],["ccc","ddd"]];
+const mergeArray = array.reduceRight((pre, current) => {
+  return pre.concat(current);
+});
+console.log(mergeArray);
