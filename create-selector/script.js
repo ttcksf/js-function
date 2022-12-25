3c91b4d0465eedf5f45213f10bb8876f46575b29
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-25 11:09:41 +0900

    add create-selector

---
+++ create-selector/script.js
@@ -0,0 +1,9 @@
+// const div = document.createElement("div");
+// console.log(div);
+// div.innerHTML = "テスト";
+// document.body.appendChild(div);
+
+// const copy = document.querySelector(".item").cloneNode(true);
+// document.querySelector(".container").appendChild(copy);
+
+
