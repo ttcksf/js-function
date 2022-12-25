c002d81c2001afbd277db08ab7c9c506cad6346d
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-25 12:31:37 +0900

    fix:handle-innerHTML

--- handle-innerText/script.js
+++ handle-innerText/script.js
@@ -7,7 +7,18 @@
 // text.textContent = "<span>おはよう</span>";
 
 const container = document.querySelector(".container");
-console.log(container);
-const attr = container.getAttribute("class");
-console.log(attr);
-container.setAttribute("id","container");
+// console.log(container);
+// const attr = container.getAttribute("class");
+// console.log(attr);
+// container.setAttribute("id","container");
+
+// container.classList.add("test");
+// container.classList.remove("test");
+// console.log(container);
+
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click",() => {
+  btn.classList.toggle("active");
+  console.log(btn);
+})
