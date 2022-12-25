ab2b5475074aa853b302f475de22b92203320b7e
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-25 11:03:06 +0900

    add add-selector

---
+++ add-selector/script.js
@@ -0,0 +1,26 @@
+// const container = document.querySelector(".container");
+// const el = document.createElement("div");
+// el.classList.add("test");
+// el.innerHTML = "テスト";
+// container.appendChild(el);
+
+//jQueryの場合
+// $('ul').append('<div class="test">テスト</div>');
+
+
+// const container = document.querySelector(".container");
+// const el = `<div class="test">テスト</div>`;
+// container.insertAdjacentHTML("afterbegin",el);
+
+//削除
+// const items = document.querySelector(".items");
+// const item = document.querySelector(".item");
+// items.removeChild(item);
+// const itemLast = document.querySelector(".item:last-child");
+// items.removeChild(itemLast);
+
+// const itemAll = document.querySelectorAll(".item");
+// const itemLength = itemAll.length;
+// for(let i = 0; i < itemLength; i++){
+//   items.removeChild(itemAll[i]);
+// }
