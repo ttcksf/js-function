19160b9c30b3dd1f058aed8fd8b638cee6bb548a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-25 10:38:27 +0900

    fix:get-selector

--- get-selector/script.js
+++ get-selector/script.js
@@ -11,8 +11,14 @@
 
 
 //複数あるときは全部出すのにAllを使う
-const li = document.querySelectorAll(".item");
-const liLength = li.length;
-for(let i = 0; i < liLength; i++){
-  console.log(li[i]);
-}
+// const li = document.querySelectorAll(".item");
+// const liLength = li.length;
+// for(let i = 0; i < liLength; i++){
+//   console.log(li[i]);
+// }
+
+// const body = document.body;
+// console.log(body);
+
+const ul = document.querySelector(".items").children;
+console.log(ul);
