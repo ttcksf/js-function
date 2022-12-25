9280b6fc93a60a2cd835f0002625c25001d93b4f
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-25 12:26:16 +0900

    add handle-innerText

---
+++ handle-innerText/script.js
@@ -0,0 +1,13 @@
+// const text = document.querySelector(".text");
+// text.textContent = "おはよう";
+
+// const text = document.querySelector(".text");
+// text.innerHTML = "おはよう";
+// text.innerHTML = "<span>おはよう</span>";
+// text.textContent = "<span>おはよう</span>";
+
+const container = document.querySelector(".container");
+console.log(container);
+const attr = container.getAttribute("class");
+console.log(attr);
+container.setAttribute("id","container");
