5dd4f5cffb513b5fec13046457dce3ebbaa6f2ec
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-01-05 16:40:23 +0900

    fix:opacity fix: scale

--- opacity/script.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector(".container");
-
-btn.addEventListener("click", () => {
-  if (container.classList.contains("show") === false) {
-    container.classList.add("show");
-  }
-});
