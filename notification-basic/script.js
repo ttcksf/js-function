c1cb2bed5d5816586de4a35775013448b131bfcd
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-01-31 01:19:14 +0900

    fix:notification-basic

--- notification-basic/script.js
+++ notification-basic/script.js
@@ -1,14 +1,21 @@
 const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
+btn.addEventListener("click", notification);
+
+// macはChromeのみ？
+// Chromeの設定からプライバシー→サイトの設定→通知を許可しておく
+// macはシステム設定からChromeの通知をオンにしておく
+// シークレットモードでは起動しない
+
+function notification() {
   switch (Notification.permission) {
     case "default":
       Notification.requestPermission();
       break;
     case "granted":
-      new Notification("こんにちは");
+      new Notification("最新のニュースです！");
       break;
     case "denied":
       alert("通知が拒否されています");
       break;
   }
-});
+}
