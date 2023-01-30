7395cada40715ac669a593442b0d5c72c958e08d
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-01-31 00:27:27 +0900

    add worker-basic

---
+++ worker-basic/script.js
@@ -0,0 +1,25 @@
+const result = document.querySelector("#result");
+const btn = document.querySelector("#btn");
+
+const worker = new Worker("worker.js");
+console.log(worker);
+
+// 1.
+// btn.addEventListener("click", () => {
+//   let total = 0;
+//   for (let i = 1; i < 2000000000; i++) {
+//     total += 1;
+//   }
+//   result.textContent = total;
+// });
+
+btn.addEventListener("click", () => {
+  worker.postMessage(0);
+  console.log("ワーカーへ依頼");
+});
+
+// 4.
+worker.onmessage = function (e) {
+  result.textContent = e.data;
+  console.log("結果を受信");
+};
---
+++ worker-basic/worker.js
@@ -0,0 +1,14 @@
+// DOM操作はできない
+// ページが開いている時しかバックグラウンドが動かない
+onmessage = (e) => {
+  // 2.
+  console.log(e.data);
+  console.log("依頼を受信");
+  let total = e.data;
+  for (let i = 1; i < 2000000000; i++) {
+    total += 1;
+  }
+  // 3.
+  console.log("結果を送信");
+  postMessage(total);
+};
