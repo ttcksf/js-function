8f40c06f570cb0572f7a1d4b24ea3dbf8a5b3e79
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-02-21 01:15:25 +0900

    add prototype-method

---
+++ prototype-method/script.js
@@ -0,0 +1,37 @@
+const array = [1, 2, 3, 4];
+// console.log(array);
+// console.dir(array);
+
+// array.forEach((val) => {
+//   console.log(val);
+// });
+
+Array.prototype.sayHello = function () {
+  console.log("hello");
+};
+// console.dir(array);
+// array.sayHello();
+
+// 同じ名前は上書きされるので危険
+// Array.prototype.sayHello = function () {
+//   console.log("test");
+// };
+// array.sayHello();
+
+// 追加するメソッド名をシンボル化すると上書きされないのでシンボルは必須（文法は変わる）
+// JavaScriptのメソッドの一覧はいつ変わるか保証がない
+
+// array.push(5);
+// console.log(array);
+// Array.prototype.push = function (val) {
+//   console.log(val);
+// };
+// array.push(5);
+const push = Symbol();
+Array.prototype[push] = function (val) {
+  console.log(val);
+};
+console.dir(array);
+array[push](5);
+array.push(5);
+console.log(array);
