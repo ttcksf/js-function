a66dd1ffed4bb61555bf6934e940c9753d6d4750
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-02-27 00:50:19 +0900

    add convert-type

---
+++ convert-type/script.js
@@ -0,0 +1,14 @@
+let num1 = "0001";
+// num1 = Number(num1);
+console.log(num1);
+console.log(num1 + 2);
+
+let num2 = 100;
+num2 = String(num2);
+console.log(typeof num2);
+console.log(num2 === 100);
+console.log(num2 === "100");
+
+let num3 = 12.4;
+num3 = parseInt(num3);
+console.log(num3);
